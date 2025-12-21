0e7aae26026100816fc8789f16dd36989d2f18ea
Rahul Shrestha <rahulshrestha3132@gmail.com>
2025-12-21 22:28:07 +0545

    Header Component

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import clsx from 'clsx';
 import { Link as LinkScroll } from 'react-scroll';
 
 const NavLink = ({ title }) => (
@@ -17,7 +17,12 @@ const Header = () => {
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div
+          className={clsx(
+            'w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0',
+            isOpen ? 'max-lg:opacity-100' : 'max-lg:pointer-events-none'
+          )}
+        >
           <div className=" max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:py-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -28,7 +33,15 @@ const Header = () => {
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      `max-lg:hidden transition-transform cursor-pointer `
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
@@ -45,6 +58,23 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
 
