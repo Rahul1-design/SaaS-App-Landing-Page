d94050a5f776941e6abfa88fe5313f723d054b6f
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-01-03 12:28:28 +0545

    Add Pricing section component with dynamic content and styling

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
         hasScrolled && 'py-2 bg-black backdrop-blur-[8px] '
       )}
     >
-      <div className="container flex h-14 items-center max-lg:pax-5">
+      <div className="container flex h-14 items-center max-lg:px-5">
         <a className=" lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
---
+++ src/sections/Pricing.jsx
@@ -0,0 +1,73 @@
+import clsx from 'clsx';
+import React, { useState } from 'react';
+import { Element } from 'react-scroll';
+import { plans } from '../constants';
+
+const Pricing = () => {
+  const [monthly, setMonthly] = useState(false);
+  return (
+    <section>
+      <Element name="pricing">
+        <div className="container">
+          <div className="max-w-960 pricing-head_before relative mx-auto border-1 border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16 ">
+            <h3 className="h3 max-lg:h4 max-md:h5 z-3 relative mx-auto mb-16 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
+              Flexible pricing for teams of all sizes
+            </h3>
+
+            <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px]">
+              <button
+                onClick={() => setMonthly(true)}
+                className={clsx('pricing-head_btn ', monthly && 'text-p4')}
+              >
+                Monthly
+              </button>
+              <button
+                onClick={() => setMonthly(false)}
+                className={clsx('pricing-head_btn ', !monthly && 'text-p4')}
+              >
+                Annual
+              </button>
+              <div
+                className={clsx(
+                  `g4 rounded-14 before:h-100 pricing-head_btn_before h-[calc(100%-16px)] w-[calc(50%-8px)]  absolute top-2 left-2 overflow-hidden shadow-400 transition-transform duration-500`,
+                  !monthly && 'translate-x-full'
+                )}
+              />
+            </div>
+
+            <div className="pricing-bg">
+              <img
+                className="relative z-2"
+                src="/images/bg-outlines.svg"
+                alt="outline"
+                width={960}
+                height={380}
+              />
+              <img
+                className="absolute inset-0 opacity-5 mix-blend-soft-light"
+                src="/images/bg-outlines-fill.png"
+                alt="outline"
+                width={960}
+                height={380}
+              />
+            </div>
+          </div>
+
+          {/* pricing section */}
+          <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6">
+            {plans.map((plan, index) => (
+              <div
+                key={plan.id}
+                className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 "
+              >
+                {plan.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Pricing;
