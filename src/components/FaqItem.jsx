7a95ad4d77c9ca36dcc119fd6c41850f53789b58
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-01-16 16:42:29 +0545

    Add background effect to FaqItem component for improved visual feedback

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -44,6 +44,15 @@ const FaqItem = ({ item, index }) => {
           <div className="body-3 px-7 py-3.5">{item.answer}</div>
         )}
       </SlideDown>
+      <div
+        className={clsx(
+          'g5 absolute -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 ',
+          active && 'opacity-100'
+        )}
+      >
+        <div className="g4 absolute inset-0.5 -z-1 rounded-3xl" />
+        <div className="bg-p1 absolute left-8 top-0 h-0.5 w-40" />
+      </div>
     </div>
   );
 };
